a4cb9255563459f86e228ff87298aaa9ef087aca
jessehartloff <hartloff@buffalo.edu>
2017-03-04 16:08:43 -0500

    initial commit

---
+++ src/AlgorithmsLecture1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class AlgorithmsLecture1 {
+
+
+    // 5 points
+    static double Q1(ArrayList<Double> input) {
+
+        // return the minimum value in the input ArrayList
+
+        return 0.0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
